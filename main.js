498f4a7400836530980bb09ef0e553011851d067
Chinwendu Agbaetuo <silviachinwendu@gmail.com>
2023-02-08 21:32:22 +0100

    theme works perfectly

---
+++ main.js
@@ -0,0 +1,69 @@
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const toggleIcon = document.getElementById("toggle-icon");
+const dropdown = document.querySelector(".dropdown");
+const select = document.querySelector(".dropdown-select");
+const caret = document.querySelector(".fas");
+const menu = document.querySelector(".dropdown-content");
+const options = document.querySelectorAll(".dropdown-content li");
+const selected = document.querySelector(".toggle-text");
+
+function toggleDarkLightMode(isDark) {
+  isDark
+    ? toggleIcon.children[0].classList.replace("fa-sun", "fa-moon")
+    : toggleIcon.children[0].classList.replace("fa-moon", "fa-sun");
+}
+
+// Switch Theme Dynamically
+function switchTheme(event) {
+  if (event.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
+    toggleDarkLightMode(true);
+    return;
+  }
+  document.documentElement.setAttribute("data-theme", "light");
+  localStorage.setItem("theme", "light");
+  toggleDarkLightMode(false);
+}
+
+// Event Listener
+toggleSwitch.addEventListener("change", switchTheme);
+
+// Check local storage for theme
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme) {
+  document.documentElement.setAttribute("data-theme", currentTheme);
+
+  if (currentTheme === "dark") {
+    toggleSwitch.checked = true;
+    toggleDarkLightMode(true);
+  }
+}
+
+
+// Display the dropdown options
+select.addEventListener("click", () => {
+  caret.classList.toggle("fa-angle-up");
+
+  menu.classList.toggle("menu-open");
+});
+
+// Select font
+options.forEach((option) => {
+  option.addEventListener("click", () => {
+    selected.innerText = option.innerText;
+    // console.log( option.innerText)
+    select.classList.remove("select-clicked");
+    caret.classList.replace("fa-angle-up", "fa-angle-down");
+    menu.classList.remove("menu-open");
+
+    options.forEach((option) => {
+      option.classList.remove("active");
+    });
+
+    option.classList.add("active");
+  });
+});
+
+
+
